7f6970424010d5ca381ae0c547b85f71a30208e5
Emily Krohn <emilykrohn2@gmail.com>
2023-10-02 20:36:15 -0700

    Stop player from moving when crane drop

--- crane.gd
+++ crane.gd
@@ -2,13 +2,16 @@ extends CharacterBody2D
 
 const CRANE_SPEED := 200.0
 
+var is_dropping := false
+
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") and not is_dropping:
 		velocity.x = -CRANE_SPEED
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right") and not is_dropping:
 		velocity.x = CRANE_SPEED
 	elif Input.is_action_pressed("ui_select"):
 		velocity.y = CRANE_SPEED
+		is_dropping = true
 	else:
 		velocity.x = 0
 	
