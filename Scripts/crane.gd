07fc60e0a3e26c8baee8d0b1ccfc731796d98046
Emily Krohn <emilykrohn2@gmail.com>
2023-10-04 20:44:55 -0700

    When times up crane auto drops

--- Scripts/crane.gd
+++ Scripts/crane.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
 const CRANE_SPEED := 200.0
-
 var is_dropping := false
+var current_timer_seconds : float
+
+func _process(delta):
+	current_timer_seconds = get_node("../Timer").get_time_left()
+	if current_timer_seconds <= 0:
+		velocity.y = CRANE_SPEED
+		is_dropping = true
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left") and not is_dropping:
