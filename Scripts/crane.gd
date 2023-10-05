7856112c9bb849c42a2d1a079e2fc3e435747d64
Emily Krohn <emilykrohn2@gmail.com>
2023-10-04 21:03:34 -0700

    Add comments to crane script

--- Scripts/crane.gd
+++ Scripts/crane.gd
@@ -6,18 +6,24 @@ var current_timer_seconds : float
 
 func _process(delta):
 	current_timer_seconds = get_node("../Timer").get_time_left()
+	# if the timer has zero seconds left, then drop the crane
 	if current_timer_seconds <= 0:
 		velocity.y = CRANE_SPEED
 		is_dropping = true
 
 func _physics_process(delta):
+	# restrict the player from moveing if the crane is dropping
 	if Input.is_action_pressed("ui_left") and not is_dropping:
 		velocity.x = -CRANE_SPEED
+		
 	elif Input.is_action_pressed("ui_right") and not is_dropping:
 		velocity.x = CRANE_SPEED
+	
+	# pressing the spacebar drops the crane
 	elif Input.is_action_pressed("ui_select"):
 		velocity.y = CRANE_SPEED
 		is_dropping = true
+	
 	else:
 		velocity.x = 0
 	
